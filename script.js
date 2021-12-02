7cb18acfd473db7b4b945d9a70a8ad61d2961165
Virusim <marianna.12344475@gmail.com>
2021-12-02 02:48:59 +0200

    com

--- script.js
+++ script.js
@@ -19,31 +19,10 @@
 alert(getMaxSubSum());
 */
 
-/*
-function fSum()
-{
-let arr = [1];  //Если нечего не записывать в массив, не работает код
-while(true)
-{
-    arr.push(prompt());
-	if(arr[arr.length-1] == null
-    || arr[arr.length-1] == ''
-    || !isFinite(arr[arr.length-1])) break;
-}
-arr.shift();
-arr.pop();
-let sum = 0;
-  for (let number of numbers) {
-    sum += number;
-  }
-  return sum;
-}
-alert(fSum);*/
-
 
 function fSum() {
 
-    let array = [1];
+    let array = [1];  //Если нечего не записывать в массив, не работает код
   
     while (true) {
   
